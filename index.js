85742f23d875b1a45040530528fe7238a4ea34d7
Carlis Liu <carlisliu@live.com>
2017-12-06 10:42:11 +0800

    add impl

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import window from 'window';
+
 function hook(target, method, wrapper) {
     if (!target) {
         return console.error('target is null');
@@ -16,4 +18,68 @@ function hook(target, method, wrapper) {
 
     let wrapped = wrapper(original);
     target[method] = wrapped;
+}
+
+var proto = window.XMLHttpRequst && window.XMLHttpRequst.prototype;
+
+hook(proto, 'open', function(open) {
+    return wrappedOpen(method, url) {
+        var store;
+        if (!(store = this._store)) {
+            store = this._store = {};
+        }
+        store.method = method;
+        store.url = url;
+        return open.apply(this, arguments);
+    }
+});
+
+hook(proto, 'send', function(send) {
+    return wrappedSend(data) {
+        var self = this;
+        self._store.dataType = (typeof data);
+        setTimeout(function() {
+            if (!self.onreadystatechange) {
+                return;
+            }
+            hook(self, 'onreadystatechange', function(onreadystatechange) {
+                return wrappedOnreadystatechange() {
+                    var store = this._store;
+                    try {
+                        var start, end;
+                        if (this.readyState == 4 && this.status == 200) {
+                            start = now();
+                        }
+                        var result = onreadystatechange.apply(this, arguments);
+                        if (this.readyState == 4 && this.status == 200) {
+                            end = now();
+                            store.callback = end - start;
+                        }
+                        return result;
+                    } catch (e) {
+                        store.error = e;
+                        setTimeout(function() {
+                            send(store);
+                        }, 0);
+                        throw e;
+                    }
+                }
+            });
+        }, 0);
+        return send.apply(this, arguments);
+    }
+});
+
+var navigator = window.navigator;
+var backend = 'http://5lym.com';
+
+function send(data) {
+    if (navigator && navigator.sendBeacon) {
+        return navigator.sendBeacon(backend, JSON && JSON.stringify ? JSON.stringify(data) : data.toString());
+    }
+    console.error('no beancon', data);
+}
+
+function now() {
+    return +new Date();
 }
